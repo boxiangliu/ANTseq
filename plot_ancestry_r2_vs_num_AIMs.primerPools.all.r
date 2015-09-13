99bee91af5a7101a3e7565e50cc15fdd26be746d
Bosh Liu <jollier.liu@gmail.com>
2015-09-13 12:00:05 -0700

    changed the code on figure theme

--- plot_ancestry_r2_vs_num_AIMs.primerPools.all.r
+++ plot_ancestry_r2_vs_num_AIMs.primerPools.all.r
@@ -7,6 +7,8 @@ suppressMessages(library(data.table))
 suppressMessages(library(dplyr))
 suppressMessages(library(stringr))
 suppressMessages(library(reshape2))
+suppressMessages(library(ggplot2))
+
 #####
 #main
 #####
@@ -24,10 +26,30 @@ for (primerPool_fname in primerPoolList){
 
 ##reshape r2List to long format: 
 r2List_long = data.frame()
-for (i in length(r2List)) {
+for (i in 1:length(r2List)) {
 	if (ncol(r2List[[i]]) == 1){
-		r2List_long = rbind(r2List_long, data.frame(r2 = r2List[[i]], pop = names(r2List)[[1]])
+		temp = data.frame(pop = names(r2List)[i], r2 = unique(r2List[[i]]))
+		temp = temp %>% mutate(npool = seq(1, nrow(temp)))
+		r2List_long = rbind(r2List_long, temp)
 	} else {
-		##TODO:melt data.frame
+		temp = unique(r2List[[i]])
+		temp = temp %>% mutate(npool = seq(1, nrow(temp)))
+		temp_long = melt(temp, id.vars = "npool", measure.vars = 1:ncol(r2List[[i]]), value.name = "r2", variable.name = "pop")
+		temp_long = temp_long %>% mutate(pop = paste(names(r2List)[i], str_replace(pop, "_r2", ""), sep = ":"))
+		r2List_long = rbind(r2List_long, temp_long)
 	}
 }
+
+
+##plot r2List_long:
+p = ggplot(r2List_long, aes(x = npool, y = r2, color = pop)) + geom_line() + geom_point() + theme_bw() + 
+	xlab('Pool number') + ylab(expression(bold(R^"2"))) + 
+	scale_color_discrete(name = 'Populations') +
+	theme(legend.position = c(1,0), legend.justification = c(1,0), legend.title = element_text(size = 15), legend.text = element_text(size = 12), axis.title = element_text(size = 20, face = 'bold'), axis.text = element_text(size = 12)) 
+
+ggsave(filename = "/srv/persistent/bliu2/ancestry/AIMS_selection/multiplexPrimers/primerPools/ancestry_r2_vs_num_pools.pdf", plot = p)
+
+
+##tabulate r2 using all pools: 
+r2max = r2List_long %>% group_by(pop) %>% summarize(r2 = max(r2), npool = length(r2)) 
+write.csv(r2max, "/srv/persistent/bliu2/ancestry/AIMS_selection/multiplexPrimers/primerPools/r2_using_all_pools.csv", row.names = FALSE)
